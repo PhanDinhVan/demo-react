f5a24a60238bd4a0027c4ff8b9d3b04b38318f44
PhanDinhVan <pdvan.it@gmail.com>
2017-12-19 14:57:58 +0700

    commit code 20171219

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
 
   state = {
     persons: [
-      {name: 'Ronaldo', age: 32},
-      {name: 'Messi', age: 30},
-      {name: 'Neymar', age: 25}
+      {id: 'qwe1', name: 'Ronaldo', age: 32},
+      {id: 'asd1', name: 'Messi', age: 30},
+      {id: 'zxc1', name: 'Neymar', age: 25}
     ],
     otherState: 'some other value',
     showPersons: false
@@ -29,21 +30,52 @@ class App extends Component {
   // }
 
   // thay doi gia tri khi nhap vao input file Person
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+
+    // lay chi so cua object can sua
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // get object in array persons theo chi so personIndex
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // set name of object vua get
+    person.name = event.target.value;
+
+    // copy lai array persons
+    const persons = [...this.state.persons];
+    // gan lai value of object in array persons = voi object vua edit
+    persons[personIndex] = person;
+
+    // set lai array
     this.setState(
       {
-        persons: [
-          {name: 'Ronaldo', age: 36},
-          {name: event.target.value, age: 34},
-          {name: 'Black', age: 38}
-        ]
+        persons: persons
       }
-    )
+    );
+
+    // this.setState(
+    //   {
+    //     persons: [
+    //       {name: 'Ronaldo', age: 36},
+    //       {name: event.target.value, age: 34},
+    //       {name: 'Black', age: 38}
+    //     ]
+    //   }
+    // )
   }
 
+  // delete row
   deletePerSonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    // copy mang persons in state
+    const persons = [...this.state.persons];
+    // delete object in array persons o vi tri la personIndex, va delete 1 object
     persons.splice(personIndex, 1);
+    // set lai array persons
     this.setState({persons: persons})
   }
 
@@ -56,11 +88,16 @@ class App extends Component {
   // <button onClick={this.switchNameHandler.bind(this, 'Drogba')}>Switch name</button>
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -91,17 +128,35 @@ class App extends Component {
               return <Person
                 click={() => this.deletePerSonHandler(index)}
                 name={person.name}
-                age={person.age}/>
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
             })
           }
         </div>
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
+    }
+
+    let classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red'); // classes = ['red']
+    }
+
+    if(this.state.persons.length <= 1){
+      classes.push('bold'); // classes = ['red', 'bold']
     }
 
     return (
       <div className="App">
         <h1>Hi, Wellcome to react!</h1>
-        <p>This is really working</p>
+        <p className={classes.join(' ')}>This is really working</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
@@ -112,4 +167,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
